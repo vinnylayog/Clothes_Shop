bab285b9804575460cf11f1b4966410c8ea1a28b
Vinny Layog <vinnylayog@gmail.com>
2023-01-12 21:57:23 +0800

    Added Inventory UI Update
    - Inventory Button added in top left
    - Items can be picked up, added and removed from inventory

---
+++ Assets/Scripts/Items/InventoryUI.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject inventoryUI;
+    public GameObject inventoryButton;
+    public Transform itemsParent;
+
+    Inventory myInventory;
+
+    InventorySlot[] slots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInventory = Inventory.Instance;
+        myInventory.onItemChangedCallback += UpdateUI;
+
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory"))
+        {
+            OpenCloseInventory();
+        }
+    }
+
+    public void OpenCloseInventory()
+    {
+        inventoryUI.SetActive(!inventoryUI.activeSelf);
+        //inventoryButton.SetActive(!inventoryButton.activeSelf);
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < myInventory.items.Count)
+            {
+                slots[i].AddItem(myInventory.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+}
